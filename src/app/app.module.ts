905a3d7eeeaad42dcaac6fa7aea582fc4d0b8109
Peter Hanley <120425761+PeterJohnHanley@users.noreply.github.com>
2024-02-25 18:05:40 +1000

    30 Add Customers Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from  './shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,10 +13,10 @@ import { CustomersModule } from './customers/customers.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CoreModule,
     SharedModule,
-    CustomersModule
+    CustomersModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
